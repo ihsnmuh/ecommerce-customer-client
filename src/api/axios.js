52b07e8eab24d14b5acdec2ed90e4268452964f3
ihsnmuh <muhammadihsan076@gmail.com>
2021-04-18 18:15:22 +0700

    before deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3000' // http://localhost:3000
+  baseURL: 'https://ecommerce-cms-mu.herokuapp.com' // http://localhost:3000
 })
